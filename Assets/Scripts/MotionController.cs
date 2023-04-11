72a0c99f3690803edc8b48917ba87c164e4a5f81
Dmitriy Bushuev <mityabushuev@gmail.com>
2023-04-11 22:10:18 +0500

    Movement
    Добавлена способность ходить персонажу

---
+++ Assets/Scripts/MotionController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MotionController : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private float deltaX;
+    private float deltaY;
+    private float moveLimeter = 0.7f;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        deltaX = Input.GetAxisRaw("Horizontal");
+        deltaY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (deltaX != 0 && deltaY != 0)
+        {
+            deltaX *= moveLimeter;
+            deltaY *= moveLimeter;
+        }
+        body.velocity = new Vector2(deltaX * speed, deltaY * speed);
+    }
+}
